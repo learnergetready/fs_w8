216f1a05847d77d8758ee9e463c0fc3930f7e023
Learner Getready <learnergit@hymyilee.fi>
2024-01-12 15:04:46 +0200

    8.5

--- booker-server/index.js
+++ booker-server/index.js
@@ -102,7 +102,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks(author: String): [Book!]!,
+    allBooks(author: String, genre: String): [Book!]!,
     allAuthors: [Author!]!,
   }
 `
@@ -111,8 +111,15 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) =>
-      args.author ? books.filter((b) => b.author === args.author) : books,
+    allBooks: (root, args) => {
+      const authoredBooks = args.author
+        ? books.filter((b) => b.author === args.author)
+        : books
+      const authoredAndGenredBooks = args.genre
+        ? authoredBooks.filter((b) => b.genres.includes(args.genre))
+        : authoredBooks
+      return authoredAndGenredBooks
+    },
     allAuthors: () => authors,
   },
   Author: {
