26fd488621d3121f16353d273d885cf5ec7804f6
Learner Getready <learnergit@hymyilee.fi>
2024-01-15 15:43:02 +0200

    8.16

--- booker-server/index.js
+++ booker-server/index.js
@@ -4,7 +4,10 @@ const { startStandaloneServer } = require("@apollo/server/standalone")
 const mongoose = require("mongoose")
 const Author = require("./models/author")
 const Book = require("./models/book")
+const User = require("./models/user")
 const { GraphQLError } = require("graphql")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 mongoose.set("strictQuery", false)
 
@@ -17,6 +20,16 @@ mongoose
     )
 
 const typeDefs = `
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  
+  type Token {
+    value: String!
+  }
+
   type Author {
     name: String!,
     born: Int,
@@ -36,6 +49,7 @@ const typeDefs = `
     authorCount: Int!,
     allBooks(author: String, genre: String): [Book!]!,
     allAuthors: [Author!]!,
+    me: User
   },
 
   type Mutation {
@@ -48,9 +62,33 @@ const typeDefs = `
     editAuthor(
       name: String!,
       setBornTo: Int!
-    ): Author
+    ): Author,
+    createUser(
+        username: String!
+        favoriteGenre: String!
+        password: String!
+      ): User
+      login(
+        username: String!
+        password: String!
+      ): Token
   }
 `
+
+const handleMutationError = (error) => {
+    let errorMessages = ""
+    if (error) {
+        if (error.name === "ValidationError") {
+            Object.values(error.errors).map(
+                (val) => (errorMessages += val.message + " "),
+            )
+            throw new GraphQLError(errorMessages, {
+                extensions: { code: "VALIDATION_ERROR" },
+            })
+        }
+    }
+    throw new GraphQLError("Some unhandeled error occured")
+}
 
 const resolvers = {
     Query: {
@@ -82,6 +120,7 @@ const resolvers = {
             }
         },
         allAuthors: async () => await Author.find({}),
+        me: (root, args, context) => context.currentUser || null,
     },
     Author: {
         bookCount: async (root) => {
@@ -96,7 +135,13 @@ const resolvers = {
         author: async (root) => Author.findById(root.author),
     },
     Mutation: {
-        addBook: async (root, args) => {
+        addBook: async (root, args, context) => {
+            if (!context.currentUser) {
+                throw new GraphQLError(
+                    "Operation allowed only for logged in users",
+                    { extensions: { code: "FORBIDDEN" } },
+                )
+            }
             try {
                 const foundAuthor = await Author.findOne({ name: args.author })
                 if (!foundAuthor) {
@@ -110,21 +155,16 @@ const resolvers = {
                 await book.save()
                 return book
             } catch (error) {
-                let errorMessages = ""
-                if (error) {
-                    if (error.name === "ValidationError") {
-                        Object.values(error.errors).map(
-                            (val) => (errorMessages += val.message + " "),
-                        )
-                        throw new GraphQLError(errorMessages, {
-                            extensions: { code: "VALIDATION_ERROR" },
-                        })
-                    }
-                }
-                throw new GraphQLError("Some unhandeled error occured")
+                handleMutationError(error)
             }
         },
-        editAuthor: async (root, args) => {
+        editAuthor: async (root, args, context) => {
+            if (!context.currentUser) {
+                throw new GraphQLError(
+                    "Operation allowed only for logged in users",
+                    { extensions: { code: "FORBIDDEN" } },
+                )
+            }
             try {
                 const foundAuthor = await Author.findOne({ name: args.name })
                 if (!foundAuthor) {
@@ -134,19 +174,41 @@ const resolvers = {
                 await foundAuthor.save()
                 return foundAuthor
             } catch (error) {
-                let errorMessages = ""
-                if (error) {
-                    if (error.name === "ValidationError") {
-                        Object.values(error.errors).map(
-                            (val) => (errorMessages += val.message + " "),
-                        )
-                        throw new GraphQLError(errorMessages, {
-                            extensions: { code: "VALIDATION_ERROR" },
-                        })
-                    }
-                }
-                throw new GraphQLError("Some unhandeled error occured")
+                handleMutationError(error)
             }
+        },
+        createUser: async (root, args) => {
+            try {
+                const { username, password, favoriteGenre } = args
+                const saltRounds = 10
+                const passwordHash = await bcrypt.hash(password, saltRounds)
+                const user = new User({ username, favoriteGenre, passwordHash })
+                const savedUser = await user.save()
+                return savedUser
+            } catch (error) {
+                handleMutationError(error)
+            }
+        },
+        login: async (root, args) => {
+            const { username, password } = args
+            const user = await User.findOne({ username })
+            const passwordCorrect =
+                user === null
+                    ? false
+                    : await bcrypt.compare(password, user.passwordHash)
+
+            if (!user || !passwordCorrect) {
+                throw new GraphQLError("Username and password did not match", {
+                    extensions: { code: "BAD_USER_INPUT" },
+                })
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id,
+            }
+
+            return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
         },
     },
 }
@@ -158,6 +220,17 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
     listen: { port: process.env.PORT },
+    context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith("Bearer ")) {
+            const decodedToken = jwt.verify(
+                auth.substring(7),
+                process.env.JWT_SECRET,
+            )
+            const currentUser = await User.findById(decodedToken.id)
+            return { currentUser }
+        }
+    },
 }).then(({ url }) => {
     console.log(`Server ready at ${url}`)
 })
